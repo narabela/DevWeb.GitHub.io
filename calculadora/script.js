c6a252a411ece6af6d0447d49ed157250148da3c
narabela <bela.lorrane32@gmail.com>
2025-04-16 21:17:28 -0300

    Update script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -5,6 +5,14 @@ function guardarValor (valor){
     display.value += valor;
 }
 
+function calcular() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Erro";
+    }
+}
+
 function limpar(){
     display.value ="";
 }
