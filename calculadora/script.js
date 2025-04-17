7911ef63636a981d3bba50e763a32b6e3ed30192
narabela <bela.lorrane32@gmail.com>
2025-04-16 21:14:45 -0300

    Update script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,11 +1,10 @@
-var caixaTexto = document.getElementById('resultado');
-console.log(caixaTexto);
-
 let display = document.getElementById("display");
 
-var memoria = " ";
-function guardarValor (value){
-    memoria += value;
-    caixaTexto.value = memoria;
-    console.log(memoria);
+
+function guardarValor (valor){
+    display.value += valor;
+}
+
+function limpar(){
+    display.value ="";
 }
